5981fd4040d5cc954617905f83610782e6dcdb9e
aalmiray <aalmiray@gmail.com>
2012-05-10 21:17:19 +0200

    First cut

--- JrebelGriffonPlugin.groovy
+++ JrebelGriffonPlugin.groovy
@@ -1,14 +1,33 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Andres Almiray
+ */
 class JrebelGriffonPlugin {
     // the plugin version
     String version = '0.1'
     // the version or versions of Griffon the plugin is designed for
-    String griffonVersion = '0.9.5 > *'
+    String griffonVersion = '0.9.6 > *'
     // the other plugins this plugin depends on
     Map dependsOn = [:]
     // resources that are included in plugin packaging
     List pluginIncludes = []
     // the plugin license
-    String license = '<UNKNOWN>'
+    String license = 'Apache Software License 2.0'
     // Toolkit compatibility. No value means compatible with all
     // Valid values are: swing, javafx, swt, pivot, qt
     List toolkits = []
@@ -19,25 +38,29 @@ class JrebelGriffonPlugin {
     // URL where documentation can be found
     String documentation = ''
     // URL where source can be found
-    String source = ''
+    String source = 'https://github.com/griffon/griffon-jrebel-plugin'
+    // Install as a framework plugin
+    boolean framework = true
 
     List authors = [
-        [
-            name: 'Your Name',
-            email: 'your@email.com'
-        ]
+            [
+                    name: 'Andres Almiray',
+                    email: 'aalmiray@yahoo.com'
+            ]
     ]
-    String title = 'Plugin summary/headline'
+    String title = "JRebel support"
     // accepts Markdown syntax. See http://daringfireball.net/projects/markdown/ for details
     String description = '''
-Brief description of Jrebel.
+[JRebel][1].
 
 Usage
-----
-Lorem ipsum
+-----
+
 
 Configuration
 -------------
-Lorem ipsum
+
+
+[1]: http://zeroturnaround.com/jrebel
 '''
 }
---
+++ scripts/_Events.groovy
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import griffon.util.Environment
+import static griffon.util.GriffonNameUtils.isBlank
+
+/**
+ * @author Andres Almiray
+ */
+
+eventRunAppStart = {
+	if(Environment.current != Environment.DEVELOPMENT) return
+	String jrebelHomeEnv = ant.project.properties['environment.JREBEL_HOME']
+	if (isBlank(jrebelHomeEnv)) {
+		println """
+			| Environmental variable JREBEL_HOME is not defined.
+			| The application will run with JRebel disabled.
+			|""".stripMargin()
+		return
+	}
+	
+	File jrebelHome = new File(jrebelHomeEnv)
+	if (!jrebelHome.exists()) {
+		println """
+			| Environmental variable JREBEL_HOME points to an non-existent path
+			|	\$JREBEL_HOME = $JREBEL_HOME
+			| The application will run with JRebel disabled.
+			|""".stripMargin()
+		return
+	}
+	
+	File jrebelJar = new File(jrebelHome, 'jrebel.jar')
+	if (!jrebelJar.exists()) {
+		println """
+			| jrebel.jar could not be found at the following path
+			|	$jrebelJar
+			| The application will run with JRebel disabled.
+			|""".stripMargin()
+		return
+	}
+	
+	if (!buildConfig.griffon.app.jvmOpts) buildConfig.griffon.app.jvmOpts = []
+	String jrebelAgent = "-javaagent:${jrebelJar.canonicalPath}".toString()
+	if (!buildConfig.griffon.app.jvmOpts.contains(jrebelAgent)) {
+		buildConfig.griffon.app.jvmOpts << jrebelAgent
+	}
+	
+	if (!buildConfig.griffon.app.javaOpts) buildConfig.griffon.app.javaOpts = []
+	for (option in buildConfig.jrebel.options) {
+		if (option) buildConfig.griffon.app.javaOpts << option
+	}
+}
